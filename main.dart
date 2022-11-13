f5fa596c4ec858d809c65426a49efcb5df1b8991
BLoushi52 <k.albloushii@hotmail.com>
2022-11-13 17:19:30 +0300

    solved questions

--- main.dart
+++ main.dart
@@ -1,6 +1,27 @@
-void main(){
-  
-  
-  
-  
+void main() {
+  Map<String, dynamic> menu = {
+    'burger': 6.5,
+    'pizza': 5,
+    'water': 1.5,
+  };
+
+  menu['chocolate cake'] = 3;
+  menu['water'] = 0.75;
+  print("Menu:\n");
+  for (var element in menu.entries) {
+    print("${element.key} -> ${element.value}KD");
+  }
+
+  const order = ['pizza', 'water', 'rice'];
+  var total = 0.0;
+  for (var item in order) {
+    final price = menu[item];
+    if (price != null) {
+      total += price;
+    } else {
+      print("$item is not on the menu");
+      // total += price;
+    }
+  }
+  print('\nTotal: \KD$total\n');
 }
